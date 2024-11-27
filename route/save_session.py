388ecdb1aafad8c630dd782685acfe60687fde0e
hyeoncheol1211 <guscjf10251211@gmail.com>
2024-11-27 12:16:47 +0900

    url 파싱 조건 추가

--- route/save_session.py
+++ route/save_session.py
@@ -2,10 +2,12 @@ from .tool.func import *
 
 def save_session():
     data = flask.request.get_json()
-    redirect_url = data.get('redirectUrl', '/')
+    redirect_url = data.get('redirectUrl')
 
-    path = '/' + redirect_url.split('://')[-1].split('/', 1)[-1]
-    # Flask의 서버 세션에 저장
-    flask.session['redirectUrl'] = path
+    if redirect_url != '':
+        flask.session['redirectUrl'] = redirect_url
+    else:
+        path = '/' + redirect_url.split('://')[-1].split('/', 1)[-1]
+        flask.session['redirectUrl'] = path
 
     return {'message': 'Session value saved'}
